5b87850bfaaa2893f7ed18f891eadb23208e9dce
oladitisodiq <Oladitisodiq@gmail.com>
2021-11-12 08:30:43 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'bouutiquexz'),
             'username' => env('DB_USERNAME', 'bouutiquexz'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'bouutiquexz'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
